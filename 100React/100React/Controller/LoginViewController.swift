84594c858881e7f1c20db4962c47f2dc3febc142
Ricardo Rodriguez <rr092293@gmail.com>
2019-09-30 22:29:33 -0700

    setup app architecture and created login view

---
+++ 100React/100React/Controller/LoginViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LoginViewController.swift
+//  100React
+//
+//  Created by Ricardo Rodriguez on 9/30/19.
+//  Copyright © 2019 Ricardo Rodriguez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class LoginViewController: UIViewController {
+    var loginView: LoginView!
+    let viewModel = LoginViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK: View setup
+    
+    func setup() {
+        setupView()
+        addButtonTarget()
+    }
+    
+    
+    
+    func setupView() {
+        let mainView = LoginView(frame: self.view.frame)
+        self.loginView = mainView
+        self.view.addSubview(loginView)
+        
+//        setRecipeDetailsViewConstraints()
+    }
+    
+    func addButtonTarget() {
+        loginView.loginButton.addTarget(self, action: #selector(loginTapped), for: .touchUpInside)
+    }
+    
+    @objc func loginTapped() {
+        viewModel.loginTapped()
+    }
+    
+
+    
+    
+}
---
+++ 100React/100React/ViewModel/LoginViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewModel.swift
+//  100React
+//
+//  Created by Ricardo Rodriguez on 9/30/19.
+//  Copyright © 2019 Ricardo Rodriguez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class LoginViewModel {
+    
+    func loginTapped() {
+        print("logging in..")
+    }
+    
+}
